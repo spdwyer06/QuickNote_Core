403deaa2f2ce1e9665ff6c359a2341ca1ea0fc56
Sean Dwyer <spdwyer06@gmail.com>
2021-04-07 15:27:32 -0400

    Add user entity

--- QuickNote_Data/ApplicationDbContext.cs
+++ QuickNote_Data/ApplicationDbContext.cs
@@ -1,12 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using QuickNote_Data.Entities;
 
 namespace QuickNote_Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
             
-        }
+        public DbSet<UserEntity> Users { get; set; }
     }
 }
