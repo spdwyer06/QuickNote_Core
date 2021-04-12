bf459c754f53f4cd0c7a3a1f3fd7a869d2c506df
Sean Dwyer <spdwyer06@gmail.com>
2021-04-12 17:44:38 -0400

    Add User Detail model

---
+++ QuickNote_Models/User/UserDetail.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace QuickNote_Models.User
+{
+    public class UserDetail
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateCreated { get; set; }
+    }
+}
