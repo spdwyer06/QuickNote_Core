b4d4565248f878cbba79da3796bff6fa572a21ba
Sean Dwyer <spdwyer06@gmail.com>
2021-04-15 12:09:21 -0400

    Add CreateNote endpoint in Note controller

--- QuickNote_Web/Controllers/NoteController.cs
+++ QuickNote_Web/Controllers/NoteController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using QuickNote_Models.Note;
+using QuickNote_Services.Note;
 
 namespace QuickNote_Web.Controllers
 {
@@ -11,5 +13,25 @@ namespace QuickNote_Web.Controllers
     [ApiController]
     public class NoteController : ControllerBase
     {
+        private readonly INoteService _service;
+
+        public NoteController(INoteService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> CreateNote([FromBody] NoteCreate model)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var wasCreated = await _service.CreateNoteAsync(model);
+
+            if(wasCreated)
+                return Ok("Your note was create");
+
+            return BadRequest("Your note could not be created");
+        }
     }
 }
