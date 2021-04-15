1d5ebc38b1fdb1a68a8d6dede1069379ca4ddb82
Sean Dwyer <spdwyer06@gmail.com>
2021-04-15 10:55:01 -0400

    Add Note Detail model

---
+++ QuickNote_Models/Note/NoteDetail.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace QuickNote_Models.Note
+{
+    public class NoteDetail
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
