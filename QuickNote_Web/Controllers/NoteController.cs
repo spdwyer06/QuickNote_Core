19d627fbc11e3d7fd56019d7341871fbc6c5768f
Sean Dwyer <spdwyer06@gmail.com>
2021-04-15 12:05:02 -0400

    Add base Note controller

---
+++ QuickNote_Web/Controllers/NoteController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuickNote_Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoteController : ControllerBase
+    {
+    }
+}
