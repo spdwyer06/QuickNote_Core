843c8a542e3659215fea5719995c7d1be8a9d44c
Sean Dwyer <spdwyer06@gmail.com>
2021-04-15 11:06:39 -0400

    Add CreateNote() in Note service/interface

--- QuickNote_Services/Note/INoteService.cs
+++ QuickNote_Services/Note/INoteService.cs
@@ -1,7 +1,10 @@
+using System.Threading.Tasks;
+using QuickNote_Models.Note;
+
 namespace QuickNote_Services.Note
 {
     public interface INoteService
     {
-        
+        Task<bool> CreateNoteAsync(NoteCreate model);
     }
 }
--- QuickNote_Services/Note/NoteService.cs
+++ QuickNote_Services/Note/NoteService.cs
@@ -1,7 +1,35 @@
+using System;
+using System.Threading.Tasks;
+using QuickNote_Data;
+using QuickNote_Data.Entities;
+using QuickNote_Models.Note;
+
 namespace QuickNote_Services.Note
 {
     public class NoteService : INoteService
     {
-        
+        private readonly ApplicationDbContext _db;
+        private readonly int _userId;
+
+        public NoteService(ApplicationDbContext db, int userId)
+        {
+            _db = db;
+            _userId = userId;
+        }
+
+        public async Task<bool> CreateNoteAsync(NoteCreate model)
+        {
+            var note = new NoteEntity
+            {
+                OwnerId = _userId,
+                Title = model.Title,
+                Content = model.Content,
+                CreatedUtc = DateTimeOffset.Now
+            };
+
+            await _db.Notes.AddAsync(note);
+            
+            return await _db.SaveChangesAsync() == 1;
+        }
     }
 }
